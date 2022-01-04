f269844bf3b76b71dc500ba1887524497134d628
magocod <magoyeison@gmail.com>
2022-01-04 07:47:39 -0400

    eslint prettier install

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     es2021: true,
     mocha: true,
   },
-  extends: "eslint:recommended",
+  extends: ["eslint:recommended", "prettier"],
   parserOptions: {
     ecmaVersion: 13,
   },
